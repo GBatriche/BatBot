589addfe81ba620f55b48a6e72a50e1ee6a47ced
GBatriche <batricheg@gmail.com>
2025-08-21 11:07:16 -0500

    initial commit

---
+++ chatbot.py
@@ -0,0 +1,10 @@
+print('Bem vindo ao BatBot (Digite X para sair)\n')
+pergunta = input('Digite sua primeira pergunta: ')
+
+while True:
+    print('Bot: aqui colocaremos a resposta do bot')
+    pergunta = input('Usuário: ')
+    if pergunta.lower() == 'x':
+        break
+
+    print('Muito obrigado por usar o BatBot')
