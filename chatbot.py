75ecf4c63715be30f31d71b27823c7e0873bf76c
GBatriche <batricheg@gmail.com>
2025-08-21 15:47:32 -0500

    imported the libs and LLM API

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,33 @@
-def resposta_bot(mensagens):
-    return 'Resposta do bot'
+import os
+from dotenv import load_dotenv
 
-print('Bem vindo ao BatBot (Digite X para sair)\n')
+from langchain_groq import ChatGroq  
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+llm = ChatGroq(
+    model="llama3-8b-8192" 
+)
+
+def resposta_bot(mensagens):
+    mensagens_modelo = [('system', 'Você é um assistente divertido chamado BatBot, que usa humor ácido para exemplificar')]
+    mensagens_modelo += mensagens
+    template = ChatPromptTemplate.from_messages(mensagens_modelo)
+    chain = template | llm
+    return chain.invoke({}).content
+
+print('Bem-vindo ao BatBot')
 
 mensagens = []
-
 while True:
-    pergunta = input('Usuário: ')
+    pergunta = input('Usuario: ')
     if pergunta.lower() == 'x':
         break
-    mensagens.append({'role': 'user', 'content' : pergunta})
+    mensagens.append(('user', pergunta))
     resposta = resposta_bot(mensagens)
-    mensagens.append({'role': 'assistant', 'content': resposta})
+    mensagens.append(('assistant', resposta))
     print(f'Bot: {resposta}')
-    print('Muito obrigado por usar o BatBot')
-    print(mensagens)
+
+print('Muito obrigado por usar o BatBot')
+print(mensagens)
