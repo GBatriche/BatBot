62e26d2611228ad979268387ece142ed3061ed8b
GBatriche <batricheg@gmail.com>
2025-08-21 13:42:45 -0500

    preraring the bot for responses

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,17 @@
+def resposta_bot(mensagens):
+    return 'Resposta do bot'
+
 print('Bem vindo ao BatBot (Digite X para sair)\n')
-pergunta = input('Digite sua primeira pergunta: ')
+
+mensagens = []
 
 while True:
-    print('Bot: aqui colocaremos a resposta do bot')
     pergunta = input('Usuário: ')
     if pergunta.lower() == 'x':
         break
-
+    mensagens.append({'role': 'user', 'content' : pergunta})
+    resposta = resposta_bot(mensagens)
+    mensagens.append({'role': 'assistant', 'content': resposta})
+    print(f'Bot: {resposta}')
     print('Muito obrigado por usar o BatBot')
+    print(mensagens)
