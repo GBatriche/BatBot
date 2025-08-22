3c8f9470aa74ff011e925b40bc173b5827c0b2e4
GBatriche <batricheg@gmail.com>
2025-08-22 09:37:13 -0500

    preparing the structure of chatbot

--- chatbot.py
+++ chatbot.py
@@ -4,11 +4,24 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq  
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import YoutubeLoader
 
 load_dotenv()
 llm = ChatGroq(
     model="llama3-8b-8192" 
 )
+
+url ='{input}'
+loader = YoutubeLoader.from_youtube_url(
+    url,
+    language=['pt']
+)
+lista_documentos = loader.load()
+
+documento = ''
+for doc in lista_documentos:
+    documento = documento + doc.page_content
+
 
 def resposta_bot(mensagens):
     mensagens_modelo = [('system', 'Você é um assistente divertido chamado BatBot, que usa humor ácido para exemplificar')]
@@ -17,7 +30,38 @@ def resposta_bot(mensagens):
     chain = template | llm
     return chain.invoke({}).content
 
+def carrega_site():
+    url_site = input('Digite a url do site: ')
+    documento = ''
+    return documento
+
+def carrega_pdf():
+    documento = ''
+    return documento
+
+def carrega_youtube():
+    url_youtube = input('Digite a url do vídeo: ')
+    documento = ''
+    return documento
+
 print('Bem-vindo ao BatBot')
+
+texto_selecao = '''
+Digite 1 se você quiser conversar com um site
+Digite 2 se você quiser conversar com um PDF
+Digite 3 se você quiser conversar com um vídeo do YouTube'''
+while True:
+    selecao = input(texto_selecao)
+    if selecao == '1':
+        documento = carrega_site()
+        break
+    if selecao == '2':
+        documento = carrega_pdf()
+        break
+    if selecao == '3':
+        documento = carrega_youtube()
+        break
+    print('Digite um valor entre 1 e 3')
 
 mensagens = []
 while True:
